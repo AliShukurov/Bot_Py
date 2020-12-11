7f1c9b06610f4250faebdfd0d489ea4cd0e54bd1
AliShukurov <75489998+AliShukurov@users.noreply.github.com>
2020-12-11 20:50:51 +0300

    11.12.2020

--- Python/bot.py
+++ Python/bot.py
@@ -2,13 +2,21 @@ import kb
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-
-
 from config import API_KEY
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
+
+storage = MemoryStorage()
 bot = Bot(token=API_KEY)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
+
+class FillInfo(StatesGroup):
+    waiting_for_course = State()
+    waiting_for_faculty = State()
+    waiting_for_group = State()
 
 
 
@@ -17,11 +25,41 @@ async def process_start_command(message: types.Message):
     await message.answer("Привет!\nЭто приветсвенное сообщение", reply_markup=kb.inline_kb1)
 
 
-#Пояснительное сообщение к заполнению расписания
+#Пояснительное сообщение к заполнению расписания(обработчик нажатия батон1)
 @dp.callback_query_handler(lambda c: c.data == 'button1')
 async def process_callback_button1(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
     await bot.send_message(callback_query.from_user.id, 'Чтобы коректно заполнить сделайт так, как указано на примере снизу\nтутутут', reply_markup=kb.inline_kb2)
+
+#Заполнение расписания по дням 
+@dp.callback_query_handler(lambda c: c.data == 'button2')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Введите курс')
+    await FillInfo.waiting_for_course.set()
+
+@dp.message_handler(state=FillInfo.waiting_for_course, content_types=types.ContentTypes.TEXT)
+async def fill_faculty(message: types.Message, state: FSMContext):
+    await state.update_data(chosen_course=message.text.lower())
+    await FillInfo.next()
+    await message.answer("Теперь введите факультет")
+
+
+@dp.message_handler(state=FillInfo.waiting_for_faculty, content_types=types.ContentTypes.TEXT)
+async def fil_group(message: types.Message, state: FSMContext):
+    await state.update_data(chosen_faculty=message.text)
+    await FillInfo.next()
+    await message.answer("Теперь введите номер группы")
+
+
+@dp.message_handler(state=FillInfo.waiting_for_group, content_types=types.ContentTypes.TEXT)
+async def show_info(message: types.Message, state: FSMContext):
+    await state.update_data(chosen_group=message.text.lower())
+    user_data = await state.get_data()
+    await message.answer(f"Вы студент {user_data['chosen_course']}-ого курса, учитесь на факультете {user_data['chosen_faculty']}\n"
+                         f"В группе {user_data['chosen_group']}")
+    await state.finish()
+
 
 
 
